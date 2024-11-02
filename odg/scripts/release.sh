cb817335642934e0f6915abbb45d0bdfa0fade21
Egezenn <egezenn@tutanota.com>
2024-11-02 23:34:25 +0300

    Oversight, updates to local release script

--- odg/scripts/release.sh
+++ odg/scripts/release.sh
@@ -2,8 +2,7 @@
 # source .venv/Scripts/activate
 # python -m pip install --upgrade pip
 # pip install -r requirements.txt
-RELEASE_NAME=$(date +'%Y-%m-%d')
-rm -r release/$RELEASE_NAME
+RELEASE_NAME=$(date +'%Y-%m-%d %H.%M.%S')
 
 python -m odg -r -c -s
 
@@ -15,9 +14,9 @@ curl -s -L -o "PREVIOUS_RELEASE.zip" https://github.com/Egezenn/OpenDotaGuides/r
 unzip -q "PREVIOUS_RELEASE.zip" -d "PREVIOUS_RELEASE"
 bash odg/scripts/diff.sh "itembuilds" "PREVIOUS_RELEASE"
 
-mkdir -p release/$RELEASE_NAME
-mv itembuilds.zip release/$RELEASE_NAME
-mv comparison.diff release/$RELEASE_NAME
+mkdir -p release/"$RELEASE_NAME"
+mv itembuilds.zip release/"$RELEASE_NAME"
+mv comparison.diff release/"$RELEASE_NAME"
 
 rm odg.log PREVIOUS_RELEASE.zip
-rm -r PREVIOUS_RELEASE
+rm -r data itembuilds PREVIOUS_RELEASE 
